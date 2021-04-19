6f79cf09cec1976c8f6a8facef118eff25326b0b
Camilo Torres <ctorres803@unab.edu.co>
2021-04-18 22:21:28 -0500

    añadiendo tailwindcss

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,31 +1,40 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
-    </head>
-    <body class="antialiased">
-       <h4>Listado de Etiquetas</h4>
-       <table>
-            <form action="tags" method="post">
-                @csrf
-                <input type="text" name="name">
-                <input type="submit" value="Agregar">
-            </form>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css" rel="stylesheet">
+
+    <title>Laravel</title>
+</head>
+
+<body class="bg-gray-200 py-10">
+    <div class="max-w-lg bg-white mx-auto p-5 rounded shadow">
+
+
+        <form action="tags" method="post" class="flex mb-4">
+            @csrf
+            <input type="text" name="name" class="rounded-l bg-gray-200 p-4 w-full outline-none" placeholder="Nueva etiqueta">
+            <input type="submit" value="Agregar" class="rounded-r px-8 bg-blue-500 text-white outline-none">
+        </form>
+
+        <h4 class="text-lg text-center mb-4">Listado de Etiquetas</h4>
+
+        <table>
             @forelse($tags as $tag)
             <tr>
-                <td>
-                    {{ $tag->name }}
-                </td>
-                <td>
+                <td class="border px-4 py-2">{{ $tag->name }}</td>
+                <td class="border px-4 py-2">{{ $tag->slug }}</td>
+
+                <td class="px-4 py-2">
                     <form action="tags/{{ $tag->id }}" method="post">
                         @csrf
                         @method('DELETE')
-                        <input type="submit" value="Eliminar">
+                        <input type="submit" value="Eliminar" class="px-4 rounded bg-red-500 text-white">
                     </form>
                 </td>
+
             </tr>
             @empty
             <tr>
@@ -34,6 +43,9 @@
                 </td>
             </tr>
             @endforelse
-       </table>
-    </body>
+        </table>
+
+    </div>
+</body>
+
 </html>
